0dda2450f2724b0165b7fa668fcc7d0ea0882a83
Vinicius Macelai <vinicius.macelai@gmail.com>
2019-06-13 14:26:20 -0300

    gaussian quadradute with boost library

--- hypothesis.cpp
+++ hypothesis.cpp
@@ -1,12 +1,18 @@
 #include "hypothesis.h"
 #include <math.h>
+#include <boost/math/quadrature/gauss.hpp> 
 	
 Hypothesis::Hypothesis(){
 		
 }
 
 double Hypothesis::testAverage(double sampleAvg, double sampleStdDev, unsigned long sampleNumElements, double confidencelevel, double avg, H1Comparition comp) {
-		double standardDeviationDistribuition = sampleStdDev / sqrt(sampleNumElements);
-		return sampleStdDev; 
+	using namespace boost::math::quadrature;
+
+	auto f = [](const double& t) { return (1.0 / sqrt (2 * M_PI)) * exp(pow(-t,2) / 2); };
+
+	double Q = gauss<double, 100>::integrate(f, 0, 1);
+
+	return Q;
 }
 
--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 int main() {
 	Hypothesis* hypo = new Hypothesis();
 	double b = 0;
-		double c = 0.5;
+	double c = 0.5;
 	double a = hypo->testAverage(b, c, b, b, b, hypo->H1Comparition::DIFFERENT);
-	printf("A %d", a);
+	printf("%f", a);
 	return 0;
 }
