849f851421b65305f2ea53c9a3351de8788dcfc8
souzarenato <souzarenato@users.noreply.github.com>
2019-06-21 01:11:08 -0300

    Add test hypothesis

--- hypothesis.cpp
+++ hypothesis.cpp
@@ -1,28 +1,64 @@
 #include "hypothesis.h"
 #include <math.h>
-#include <boost/math/quadrature/gauss.hpp> 
-	
-Hypothesis::Hypothesis(){
-		
+#include <boost/math/quadrature/gauss.hpp>
+
+Hypothesis::Hypothesis() = default;
+
+double
+Hypothesis::testAverage(double sampleAvg, double sampleStdDev, unsigned long sampleNumElements, double confidencelevel,
+                        double avg, H1Comparition comp) {
+    using namespace boost::math::quadrature;
+
+    double stdError = sampleStdDev / sqrt(sampleNumElements);
+    printf("   # stdError=%f ", stdError);
+
+    double z_obs = (sampleAvg - avg) / stdError;
+    printf("# z_obs=%f ", z_obs);
+
+    auto f = [](const double& x) {
+        double constant = 1 / sqrt(2 * M_PI);
+        double e = exp(pow(x, 2));
+        double sqrtE = 1 / sqrt(e);
+        return constant * sqrtE;
+    };
+
+    double Q = gauss<double, 10000>::integrate(f, -INFINITY, z_obs);
+
+    double alpha = confidencelevel/100;
+    double cl = 1 - alpha;
+
+    if (comp == H1Comparition::DIFFERENT) {
+
+        double alpha_half = alpha/2;
+        double z_critico = cl + alpha_half;
+
+        if (z_obs > z_critico) {
+            printf("\n   # REJEITAR H0 => MEDIA NAO Eh IGUAL A %0.4f NIVEL DE SIGNIFICANCIA", alpha);
+        } else {
+            printf("\n   # ACEITA H0 => MEDIA NAO Eh IGUAL A %0.4f NIVEL DE SIGNIFICANCIA", alpha);
+        }
+
+        return Q;
+
+    } else if (comp == H1Comparition::LESS_THAN) {
+
+        if (-z_obs > -cl ) {
+            printf("\n   # REJEITAR H0 => MEDIA NAO Eh MAIOR IGUAL A %0.4f NIVEL DE SIGNIFICANCIA", alpha);
+        } else {
+            printf("\n   # ACEITA H0 => MEDIA NAO Eh MAIOR IGUAL A %0.4f NIVEL DE SIGNIFICANCIA", alpha);
+        }
+
+        return -Q;
+
+    } else {
+
+        if (z_obs > cl) {
+            printf("\n   # REJEITAR H0 => MEDIA NAO Eh MENOR IGUAL A %0.4f NIVEL DE SIGNIFICANCIA", alpha);
+        } else {
+            printf("\n   # ACEITA H0 => MEDIA NAO Eh MENOR IGUAL A %0.4f NIVEL DE SIGNIFICANCIA", alpha);
+        }
+
+        return Q;
+    }
+
 }
-
-double Hypothesis::testAverage(double sampleAvg, double sampleStdDev, unsigned long sampleNumElements, double confidencelevel, double avg, H1Comparition comp) {
-	using namespace boost::math::quadrature;
-
-	double stdError = sampleStdDev / sqrt(sampleNumElements);
-	printf(" stdError=%f", stdError);
-
-	double t = (sampleAvg - avg) / stdError;
-	printf(" t=%f", t);
-
-	auto f = [](const double& t) {
-		double constant = 1 / sqrt(2 * M_PI);
-		double e = exp(pow(t, 2));
-		double sqrtE = 1 / sqrt(e);
-		return constant * sqrtE;
-	};
-
-	double Q = gauss<double, 10000>::integrate(f, -1000000, t);
-	return Q;
-}
-
--- main.cpp
+++ main.cpp
@@ -2,8 +2,22 @@
 #include <stdio.h>
 
 int main() {
-	Hypothesis* hypo = new Hypothesis();
-	double a = hypo->testAverage(0.81, 0.0031, 41, 10, 0.787, hypo->H1Comparition::DIFFERENT);
-	printf(" p: %f", a);
-	return 0;
+    Hypothesis *hypo = new Hypothesis();
+
+    double sampleAvg = 118;
+    double sampleStdDev = 20;
+    unsigned long sampleNumElements = 20;
+    double confidencelevel = 5;
+    double avg = 115;
+
+    printf("-> Test Average \n");
+    double a = hypo->testAverage(sampleAvg, sampleStdDev, sampleNumElements, confidencelevel, avg,
+                                 hypo->H1Comparition::DIFFERENT);
+    printf("\n   # p=%f", a);
+
+
+
+    return 0;
 }
+
+
